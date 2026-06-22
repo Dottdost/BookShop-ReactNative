41d9222ea3913b722bc302dfe7fc3b574faa8a53
Gama <elikosamed@gmail.com>
2026-06-22 14:28:42 +0400

    Initialize i18n in root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,19 @@
 import { ThemeProvider } from "@/context/ThemeContext";
+import { initI18n } from "@/i18n";
 import { Slot } from "expo-router";
+import { useEffect, useState } from "react";
 
 export default function RootLayout() {
+  const [isI18nReady, setIsI18nReady] = useState(false);
+
+  useEffect(() => {
+    initI18n().finally(() => setIsI18nReady(true));
+  }, []);
+
+  if (!isI18nReady) {
+    return null;
+  }
+
   return (
     <ThemeProvider>
       <Slot />
