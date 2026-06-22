59e85c296fc192b3919f6f26724f26a0a82f328b
Gama <elikosamed@gmail.com>
2026-06-22 14:25:56 +0400

    Add Russian translations

---
+++ i18n/ru.ts
@@ -0,0 +1,39 @@
+const ru = {
+  app: {
+    name: "Cheshire Shelf",
+  },
+  language: {
+    label: "Язык",
+    short: "RU",
+    english: "Английский",
+    russian: "Русский",
+    azerbaijani: "Азербайджанский",
+  },
+  navigation: {
+    home: "Cheshire Shelf",
+    books: "Книги",
+    about: "О нас",
+    contacts: "Контакты",
+    profile: "Профиль",
+    wishlist: "Избранное",
+    cart: "Корзина",
+    signIn: "Войти",
+    signUp: "Регистрация",
+    book: "Книга",
+    checkout: "Оформление заказа",
+    admin: "Панель администратора",
+  },
+  common: {
+    loading: "Загрузка...",
+    save: "Сохранить",
+    cancel: "Отмена",
+    close: "Закрыть",
+    delete: "Удалить",
+    edit: "Редактировать",
+    add: "Добавить",
+    search: "Поиск",
+    back: "Назад",
+  },
+};
+
+export default ru;
